2e4d5e2adf92abf29381bb7ee2e7225f74ddf9b2
simbouch <simplonbouchaib@gmail.com>
2024-11-28 14:25:06 +0100

    third commit

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,23 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, render_template, request, jsonify
 from app.model import predict_expenses
 
 routes = Blueprint('routes', __name__)
 
+@routes.route('/')
+def index():
+    return render_template('index.html', title="Home - Medical Expenses Prediction")
+
 @routes.route('/predict', methods=['POST'])
 def predict():
-    data = request.json
-    prediction = predict_expenses(data)
-    return jsonify({"prediction": prediction})
+    try:
+        data = {
+            "age": int(request.form['age']),
+            "bmi": float(request.form['bmi']),
+            "children": int(request.form['children']),
+            "smoker": int(request.form['smoker']),
+            "region": request.form['region']
+        }
+        prediction = predict_expenses(data)
+        return render_template('result.html', title="Prediction Result", prediction=prediction)
+    except Exception as e:
+        return render_template('error.html', title="Error", error_message=str(e))
